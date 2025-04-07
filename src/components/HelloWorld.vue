dc946eb77a52bdc54ecaa2da80127d077451ba3d
Salauddin Sakib <salauddinsakib99@gmail.com>
2025-04-07 16:22:34 +0600

    navbar creae done

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,27 @@
 
 <template>
-  <div class="">
-    <h1 class="text-6xl">This is Body Page</h1>
-  </div>
+  <!-- Header -->
+  <section class="container">
+    <!-- navbar -->
+    <div class="flex justify-between items-center my-[26px]">
+      <div class="flex items-center gap-2.5 cursor-pointer">
+        <img src="../assets/img/logo.png" alt="">
+        <h2 class="text-[22px] font-bold">My_Tutor</h2>
+      </div>
+      <div class="flex items-center gap-9 text-base">
+        <ul class="flex gap-11 text-[#424352]">
+          <li class="hover:text-[#1b99d4]"><a href="#">About Us</a></li>
+          <li class="hover:text-[#1b99d4]"><a href="#">Courses</a></li>
+          <li class="hover:text-[#1b99d4]"><a href="#">Events</a></li>
+          <li class="hover:text-[#1b99d4]"><a href="#">Blogs</a></li>
+        </ul>
+        <button class="bg-[#1b99d4] text-white font-semibold py-2.5 px-5 rounded-[5px] cursor-pointer hover:bg-[#1b99d4d2]">Apply
+          Now</button>
+      </div>
+    </div>
+
+    <!-- Header -->
+    <div class="bg-[#1b99d4]"></div>
+  </section>
 </template>
 
